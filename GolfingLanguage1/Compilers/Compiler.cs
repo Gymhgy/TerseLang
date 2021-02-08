b715addcddfa3c9221332c6e9615edee6ecd40f0
Yifan Wang <cheetahyf@gmail.com>
2021-02-08 14:44:44 -0800

    string compression algo 1 finished, not very effective

---
+++ GolfingLanguage1/Compilers/Compiler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerseLang.Compilers {
+    public abstract class Compiler {
+
+        public abstract string Compile(string program);
+
+        
+    }
+}
--- UnitTestProject1/StringCompressionTests.cs
+++ UnitTestProject1/StringCompressionTests.cs
@@ -13,16 +13,17 @@ namespace TerseLang.Tests {
     public class StringCompressionTests {
         [TestMethod]
         public void CompressionAlgoA() {
-            foreach (string src in File.ReadAllLines("UnitTestProject1/StringCompressionTestsCases.txt")) {
+            foreach (string src in File.ReadAllLines(@"C:\Users\GGMS-Hank\source\repos\TerseLang\UnitTestProject1\StringCompressionTestCases.txt")) {
                 Assert.AreEqual(Tokenizer.DecompressionAlgoA(Tokenizer.CompressionAlgoA(src)), src, Tokenizer.CompressionAlgoA(src));
             }
         }
 
         [TestMethod]
         public void CompressionComparison() {
-            foreach (string src in File.ReadAllLines("UnitTestProject1/StringCompressionTestsCases.txt")) {
+            Console.WriteLine(Constants.DICTIONARY.Count);
+            foreach (string src in File.ReadAllLines(@"C:\Users\GGMS-Hank\source\repos\TerseLang\UnitTestProject1\StringCompressionTestCases.txt")) {
                 var compressedA = Tokenizer.CompressionAlgoA(src);
-                Console.WriteLine("'{0}' -> {1}\nCompression rate: {2}", src, compressedA, compressedA.Length / src.Length);
+                Console.WriteLine("'{0}' -> {1}\nCompression rate: {2}", src, compressedA, (double)compressedA.Length / src.Length);
             }
         }
     }
